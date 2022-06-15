97a991d2b757192554a99b11581934e05b649652
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2022-06-15 16:54:08 +0300

    darkMode

---
+++ 11-dark-mode/src/data.js
@@ -0,0 +1,32 @@
+const articles = [
+  {
+    id: 1,
+    title: 'Hunain Durrani',
+    date: new Date(2020, 9, 4),
+    length: 11,
+    snippet: `Arge - Director `,
+  },
+  {
+    id: 2,
+    title: 'Oğuzhan Orhan',
+    date: new Date(2019, 10, 22),
+    length: 5,
+    snippet: `Full Stack Software Engineer`,
+  },
+  {
+    id: 3,
+    title: 'Zeynep Ulucay',
+    date: new Date(2018, 7, 11),
+    length: 5,
+    snippet: `Test Engineer`,
+  },
+  {
+    id: 4,
+    title: 'Ceyda Aydın',
+    date: new Date(2015, 5, 4),
+    length: 5,
+    snippet: `Quality Engineer`,
+  },
+];
+
+export default articles;
